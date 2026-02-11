6adf1b4b57f2181d6c660f63ffd0d7ebaa967ac3
shanvvn <mhdshanmomi@email.com>
2026-02-11 10:50:02 +0530

    1

--- part 0/exercise04.m
+++ part 0/exercise04.m
@@ -1,17 +1,36 @@
+```mermaid
 sequenceDiagram
     participant browser
     participant server
 
-    Note right of browser: User writes a note in the text field and clicks Save button
-    
-    Note right of browser: JavaScript prevents default form submission and handles the event
-    
-    Note right of browser: JavaScript adds the new note to the notes array and re-renders the list
-    
-    browser->>server: POST https://studies.cs.helsinki.fi/exampleapp/new_note_spa
+    browser->>server: POST https://studies.cs.helsinki.fi/exampleapp/new_note
     activate server
-    Note left of server: Server saves the new note
-    server-->>browser: {"message": "note created"}
+    server-->>browser: HTTP 302 Found
     deactivate server
-    
-    Note right of browser: The browser stays on the same page, no reload needed. The new note is already visible.
+
+    Note right of browser: The server asks the browser to do a new HTTP GET request to the address defined in the header Location
+
+    browser->>server: GET https://studies.cs.helsinki.fi/exampleapp/notes
+    activate server
+    server-->>browser: HTML document
+    deactivate server
+
+    browser->>server: GET https://studies.cs.helsinki.fi/exampleapp/main.css
+    activate server
+    server-->>browser: the css file
+    deactivate server
+
+    browser->>server: GET https://studies.cs.helsinki.fi/exampleapp/main.js
+    activate server
+    server-->>browser: the JavaScript file
+    deactivate server
+
+    Note right of browser: The browser starts executing the JavaScript code that fetches the JSON from the server
+
+    browser->>server: GET https://studies.cs.helsinki.fi/exampleapp/data.json
+    activate server
+    server-->>browser: [{ "content": "HTML is easy", "date": "2023-1-1" }, ... ]
+    deactivate server
+
+    Note right of browser: The browser executes the callback function that renders the notes
+```
