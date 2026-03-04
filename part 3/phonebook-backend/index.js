a9b75083c9fb9129821f382e08b082c99b9e4895
shanvvn <mhdshanmomi@email.com>
2026-03-04 10:22:50 +0530

    111

--- part 3/phonebook-backend/index.js
+++ part 3/phonebook-backend/index.js
@@ -100,6 +100,12 @@ const errorHandler = (error, request, response, next) => {
     next(error)
 }
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 app.use(errorHandler)
 
 const PORT = process.env.PORT
