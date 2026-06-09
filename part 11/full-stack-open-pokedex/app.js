4e2c06abf86b46b5f53eb0c870f27b30c2b14ad3
shanvvn <mhdshanmomi@email.com>
2026-06-09 14:54:00 +0530

    Fix Part 11: Add Pokedex App and fix CI/CD path #skip

---
+++ part 11/full-stack-open-pokedex/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+// get the port from env variable
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static("dist"));
+
+app.get("/health", (req, res) => {
+  res.send("ok");
+});
+
+app.get("/version", (req, res) => {
+  res.send("1"); 
+});
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
